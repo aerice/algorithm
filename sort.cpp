041ccf83055d1a44ff55667118bd083076bd8247
wtisme <gakki9712@gmail.com>
2020-03-23 11:33:40 +0800

    sort

---
+++ sort.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+void  selection_sort(std::vector<int> &A) {
+
+}
+
+void insertion_sort(std::vector<int> &A) {
+
+}
+
+
+int main() {
+  std::ios::sync_with_stdio(false);
+  int a = 1, b = 2;
+  std::cout << a + b << std::endl;
+  return 0;
+}
